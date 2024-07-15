480f92256fdc9e13a8df281f607ace3ece2d2b1e
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-07-15 15:16:43 +0100

    Post request access the Carbon API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.scss";
 
+const url = 'https://carbonsutra1.p.rapidapi.com/vehicle_estimate_by_model';
+const data = new FormData();
+data.append('vehicle_make', 'Ford');
+data.append('vehicle_model', 'Aerostar Wagon');
+data.append('distance_value', '42');
+data.append('distance_unit', 'mi');
+
+const options = {
+	method: 'POST',
+	headers: {
+		'x-rapidapi-key': import.meta.env.VITE_X_RAPIDAPI_KEY,
+		'x-rapidapi-host': import.meta.env.VITE_X_RAPIDAPI_HOST,
+		Authorization: import.meta.env.VITE_AUTHORIZATION
+	},
+	body: data
+};
+
+let kg;
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	kg = result.data.co2e_kg;
+	console.log(result.data.co2e_kg)
+} catch (error) {
+	console.error(error);
+}
+
 function App() {
-	return <h1>Welcome to Eco Leap</h1>;
+	return <h1>{kg}</h1>;
 }
 
 export default App;
