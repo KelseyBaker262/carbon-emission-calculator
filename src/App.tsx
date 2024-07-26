78414a0c2bb8dc25e60cc2478fac1a7abfca958a
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-07-26 17:00:27 +0100

    Color Scheme and font added

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import fetchData from "./assets/components/api_call/api_call";
 
 function App() {
 	function makeCall() {
-	const result = fetchData("car")
-	console.log(result)
-		}
+		const result = fetchData("car");
+		console.log(result);
+	}
 
 	return (
 		<>
-		<h1>Welcome to Eco Leap</h1>
-		<button type="button" onClick={makeCall}>Click Me</button>
+			<h1>Welcome to Eco Leap</h1>
+			<button type="button" onClick={makeCall}>
+				API Call
+			</button>
 		</>
 	);
 }
